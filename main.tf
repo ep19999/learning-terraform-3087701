a9aa0db330b1ddaf84d45a4eb1adc94b24090c6e
ep19999 <109234737+ep19999@users.noreply.github.com>
2023-03-21 14:19:35 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -48,7 +48,7 @@ module "blog_sg" {
   
   vpc_id              = module.blog_vpc.vpc_id
   name                = "blog"
-  ingress_rules       = ["http-443-tcp","https-80-tcp"]
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
