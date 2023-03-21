d00b1dc3bdc462bc103a893b99316df1ea714fb1
ep19999 <109234737+ep19999@users.noreply.github.com>
2023-03-21 14:14:27 +0000

    module vpc

--- main.tf
+++ main.tf
@@ -32,9 +32,9 @@ module "vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
   
   tags = {
@@ -45,15 +45,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name = "blog_new"
   
-  vpc_id = module.vpc.public_subnets[0]
-  
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  vpc_id              = module.blog_vpc.vpc_id
+  name                = "blog"
+  ingress_rules       = ["http-443-tcp","https-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 
